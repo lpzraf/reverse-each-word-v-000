e5a952a6e15e8bc03318f00500fa0238e91621c9
Rafael Lopez <lpz.raf@gmail.com>
2018-07-22 15:53:17 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,21 +1,3 @@
-# def reverse_each_word (sentence1)
-#     temp_array = []
-#     reverse_array = []
-
-#     i = sentence1.length
-
-# temp_array.push(sentence1.split(""))
-
-# temp_array.each do |item|
-
-#     until i < 0
-#         reverse_array.push(item[i])
-#         i -= 1
-#     end
-# end
-# reverse_array.join
-# end
-
 def reverse_each_word (sentence1)
  strings = sentence1.split(' ')
  reversed_strings = []
@@ -28,6 +10,8 @@ def reverse_each_word (sentence1)
  end
  return reversed_strings.join(' ')
 end
+
+# "a".concat("b")
     
 
 
