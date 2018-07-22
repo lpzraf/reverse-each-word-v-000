203a29871e275423627245746d3b435546d756ee
Rafael Lopez <lpz.raf@gmail.com>
2018-07-22 16:16:06 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -17,15 +17,10 @@ def reverse_each_word (sentence1)
  strings = sentence1.split(' ')
  reversed_strings = []
  
- strings.each do |word|
-  new_string = ""
-  word.each do |char|
-  char.concat(new_string)
- end
- reversed_strings << new_string
- 
- end
- return reversed_strings.join(' ')
+strings.each do |word| 
+  reversed_strings << word.reverse
+end
+return reversed_strings.join(' ')
 end
 
 
