ffb7bdd7dd4a790a1b10f70a3a6b45937cefd6ec
Rafael Lopez <lpz.raf@gmail.com>
2018-07-22 16:19:44 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,3 @@
-# def reverse_each_word (sentence1)
-# strings = sentence1.split(' ')
-# reversed_strings = []
- 
-# strings.each do |word|
-#   new_string = ""
-#   word.each do |char|
-#   char.concat(new_string)
-# end
-# reversed_strings << new_string
- 
-# end
-# return reversed_strings.join(' ')
-# end
-
 def reverse_each_word (sentence1)
  strings = sentence1.split(' ')
  reversed_strings = []
@@ -24,15 +9,12 @@ return reversed_strings.join(' ')
 end
 
 
-# reversed_strings = []
-# strings.each do |word| #where strings is an array of words
-#   reversed_strings << word.reverse
-# end
-# #now return array rejoined as a string here.
-# return reversed_strings.join(' ')
-# end
-
-
-    
-
-
+def reverse_each_word (sentence1)
+ strings = sentence1.split(' ')
+ reversed_strings = []
+ 
+strings.collect do |word| 
+  reversed_strings << word.reverse
+end
+return reversed_strings.join(' ')
+end
