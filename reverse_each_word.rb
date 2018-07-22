3dc3a085553be038392d5be2b77e6b775339e561
Rafael Lopez <lpz.raf@gmail.com>
2018-07-22 16:24:16 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,33 +1,20 @@
-# def reverse_each_word (sentence1)
-#     temp_array = []
-#     reverse_array = []
+def reverse_each_word (sentence1)
+ strings = sentence1.split(' ')
+ reversed_strings = []
+ 
+strings.each do |word| 
+  reversed_strings << word.reverse
+end
+return reversed_strings.join(' ')
+end
 
-#     i = sentence1.length
-
-# temp_array.push(sentence1.split(""))
-
-# temp_array.each do |item|
-
-#     until i < 0
-#         reverse_array.push(item[i])
-#         i -= 1
-#     end
-# end
-# reverse_array.join
-# end
 
 def reverse_each_word (sentence1)
  strings = sentence1.split(' ')
  reversed_strings = []
  
- strings.each do |word|
-   new_string = ""
-  word.each do |char|
- end
- reversed_strings << new_string
- end
- return reversed_strings.join(' ')
+strings.collect do |word| 
+  reversed_strings << word.reverse
 end
-    
-
-
+return reversed_strings.join(' ')
+end
