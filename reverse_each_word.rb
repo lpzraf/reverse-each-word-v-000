1029661864f58d9bffc0ef71120d1c9593cd1e90
Rafael Lopez <lpz.raf@gmail.com>
2018-07-22 02:44:54 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,38 @@
+
+# def reverse_each_word (sentence1)
+#   sentence1.each do |sentence|
+#   sentence.reverse
+#   end
+# end
+
+# def reverse_each_word (sentence1)
+#     temp_array = []
+#     reverse_array = []
+
+#     i = sentence1.length
+
+# temp_array.push(sentence1.split(""))
+
+# temp_array.each do |item|
+
+#     until i < 0
+#         reverse_array.push(item[i])
+#         i -= 1
+#     end
+# end
+# reverse_array.join
+# end
+
+def reverse_each_word (sentence1)
+ strings = sentence1.split(' ')
+ reversed_string = []
+ 
+ strings.each do |word|
+  word.each do |char|
+ end
+ 
+ return reversed_string.join(' ')
+end
+    
+
+
